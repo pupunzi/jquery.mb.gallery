b8394ced747453982a786696a545cf76c0e526be
pupunzi <matteo@open-lab.com>
2013-01-04 17:35:16 +0100

    various

--- inc/mbGallery.js
+++ inc/mbGallery.js
@@ -1,22 +1,24 @@
 /*
  * ******************************************************************************
  *  jquery.mb.components
+ *  file: mbGallery.js
  *
- *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
+ *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi);
+ *  Open lab srl, Firenze - Italy
  *  email: matteo@open-lab.com
- *  site: http://pupunzi.com
+ *  site: 	http://pupunzi.com
+ *  blog:	http://pupunzi.open-lab.com
+ * 	http://open-lab.com
  *
  *  Licences: MIT, GPL
  *  http://www.opensource.org/licenses/mit-license.php
  *  http://www.gnu.org/licenses/gpl.html
+ *
+ *  last modified: 04/01/13 17.13
  *  *****************************************************************************
  */
 
 /*
- * Name:jquery.mb.gallery
- * Version: 2.1
- *
- *
  * It is possible to show EXIF metadata of your photos.
  * include: jquery.exif.js (http://www.nihilogic.dk/labs/exifjquery/)
  * set exifData: true
