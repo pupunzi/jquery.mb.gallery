0e53670ca8a4252af0ece3c76d89129b9281f72b
mb.jquery.components <matteo@open-lab.com>
2015-05-18 00:31:46 +0200

    various fix

--- inc/jquery.mb.thumbGallery.js
+++ inc/jquery.mb.thumbGallery.js
@@ -47,6 +47,7 @@
 			jQuery.extend(opt, jQuery.thumbGrid.defaults, options);
 
 			return this.each(function () {
+
 				var grid = this;
 				var $grid = jQuery(grid);
 
@@ -102,185 +103,6 @@
 			})
 		},
 
-		normalizeCss: function (opt) {
-
-			if (jQuery.browser.msie && jQuery.browser.version <= 9)
-				return opt;
-
-			var newOpt = jQuery.extend(true, {}, opt);
-			var sfx = "";
-			var transitionEnd = "transitionEnd";
-			if (jQuery.browser.webkit) {
-				sfx = "-webkit-";
-				transitionEnd = "webkitTransitionEnd";
-			} else if (jQuery.browser.mozilla) {
-				sfx = "-moz-";
-				transitionEnd = "transitionend";
-			} else if (jQuery.browser.opera) {
-				sfx = "-webkit-";
-				transitionEnd = "oTransitionEnd";
-			} else if (jQuery.browser.msie) {
-				sfx = "-ms-";
-				transitionEnd = "msTransitionEnd";
-			}
-
-			function uncamel(str) {
-				return str.replace(/([A-Z])/g, function (letter) { return '-' + letter.toLowerCase(); });
-			}
-
-			function setUnit(i, units) {
-				if ((typeof i === "string") && (!i.match(/^[\-0-9\.]+$/))) {
-					return i;
-				} else {
-					return "" + i + units;
-				}
-			}
-
-			for (var o in newOpt) {
-
-				if (o === "transform") {
-					newOpt[sfx + "transform"] = newOpt[o];
-					delete newOpt[o];
-				}
-
-				if (o === "transform-origin") {
-					newOpt[sfx + "transform-origin"] = opt[o];
-					delete newOpt[o];
-				}
-
-				if (o === "filter") {
-					newOpt[sfx + "filter"] = opt[o];
-					delete newOpt[o];
-				}
-
-				/**
-				 * Translate
-				 * */
-
-				var key = "";
-
-				if (o === "x") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" translateX(" + setUnit(opt[o], "px") + ")");
-					delete newOpt[o];
-				}
-
-				if (o === "y") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" translateY(" + setUnit(opt[o], "px") + ")");
-					delete newOpt[o];
-				}
-
-				if (o === "z") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" translateZ(" + setUnit(opt[o], "px") + ")");
-					delete newOpt[o];
-				}
-
-				/**
-				 * Rotate
-				 * */
-				if (o === "rotate") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" rotate(" + setUnit(opt[o], "deg") + ")");
-					delete newOpt[o];
-				}
-
-				if (o === "rotateX") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" rotateX(" + setUnit(opt[o], "deg") + ")");
-					delete newOpt[o];
-				}
-
-				if (o === "rotateY") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" rotateY(" + setUnit(opt[o], "deg") + ")");
-					delete newOpt[o];
-				}
-
-				if (o === "rotateZ") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" rotateZ(" + setUnit(opt[o], "deg") + ")");
-					delete newOpt[o];
-				}
-
-				/**
-				 * Scale
-				 * */
-				if (o === "scale") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" scale(" + setUnit(opt[o], "") + ")");
-					delete newOpt[o];
-				}
-
-				if (o === "scaleX") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" scaleX(" + setUnit(opt[o], "") + ")");
-					delete newOpt[o];
-				}
-
-				if (o === "scaleY") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" scaleY(" + setUnit(opt[o], "") + ")");
-					delete newOpt[o];
-				}
-
-				if (o === "scaleZ") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" scaleZ(" + setUnit(opt[o], "") + ")");
-					delete newOpt[o];
-				}
-
-				/**
-				 * Skew
-				 * */
-
-				if (o === "skew") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" skew(" + setUnit(opt[o], "deg") + ")");
-					delete newOpt[o];
-				}
-
-				if (o === "skewX") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" skewX(" + setUnit(opt[o], "deg") + ")");
-					delete newOpt[o];
-				}
-
-				if (o === "skewY") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" skewY(" + setUnit(opt[o], "deg") + ")");
-					delete newOpt[o];
-				}
-
-				/**
-				 * Perspective
-				 * */
-				if (o === "perspective") {
-					key = sfx + "transform";
-					newOpt[key] = newOpt[key] || "";
-					newOpt[key] += (" perspective(" + setUnit(opt[o], "px") + ")");
-					delete newOpt[o];
-				}
-
-			}
-			return newOpt;
-		},
-
 		drawPage: function (el, pageIdx, applyEffect) {
 
 			if (typeof applyEffect === "undefined")
@@ -312,10 +134,10 @@
 
 					if (applyEffect) {
 						thumbWrapper.css({opacity: 0});
-						var transitionIn = jQuery.thumbGrid.normalizeCss(jQuery.thumbGrid.transitions[grid.nav_effect].in);
+						var transitionIn = jQuery.normalizeCss(jQuery.thumbGrid.transitions[grid.nav_effect].in);
 						thumbWrapper.css(transitionIn);
 					} else {
-						var displayProperties = jQuery.thumbGrid.normalizeCss({top: 0, left: 0, opacity: 1, x: 0, y: 0, scale: 1, rotate: 0, skew: 0, filter: " blur(0)"});
+						var displayProperties = jQuery.normalizeCss({top: 0, left: 0, opacity: 1, x: 0, y: 0, scale: 1, rotate: 0, skew: 0, filter: " blur(0)"});
 						thumbWrapper.css(displayProperties).show();
 					}
 
@@ -378,7 +200,7 @@
 						grid.nav.show();
 				}
 
-			}, 10);
+			}, 100);
 
 			jQuery(window).on("resize.thumbGrid", function () {
 				grid.height = $page.height();
@@ -490,10 +312,9 @@
 				},
 
 				keyboard: function (on) {
+
 					if (on) {
 						jQuery(document).on("keydown.thumbGallery", function (e) {
-
-//							console.debug(e.keyCode, e.key) e.key is not supported by Webkit
 
 							switch (e.keyCode) {
 
@@ -549,13 +370,13 @@
 					var displayProperties = {top: 0, left: 0, opacity: 1, x: 0, y: 0, scale: 1, rotate: 0, skew: 0, filter: "blur(0)"};
 
 					if (animate) {
-						imgWrapper.css(jQuery.thumbGrid.normalizeCss(jQuery.thumbGrid.transitions[slideShow.effect].in));
+						imgWrapper.css(jQuery.normalizeCss(jQuery.thumbGrid.transitions[slideShow.effect].in));
 						slideShow.spinner = setTimeout(function () {
 							spinner = new Spinner(spinnerOpts).spin(target);
 							spinnerPh.fadeIn(300);
 						}, 1000)
 					} else {
-						displayProperties = jQuery.thumbGrid.normalizeCss(displayProperties);
+						displayProperties = jQuery.normalizeCss(displayProperties);
 						imgWrapper.css(displayProperties);
 						imgWrapper.css({opacity: 0});
 					}
@@ -568,9 +389,9 @@
 						this.loaded = true;
 
 						if (animate) {
-							imgWrapper.css(jQuery.thumbGrid.normalizeCss(jQuery.thumbGrid.transitions[slideShow.effect].in));
+							imgWrapper.css(jQuery.normalizeCss(jQuery.thumbGrid.transitions[slideShow.effect].in));
 						} else {
-							displayProperties = jQuery.thumbGrid.normalizeCss(displayProperties);
+							displayProperties = jQuery.normalizeCss(displayProperties);
 							imgWrapper.css(displayProperties);
 							imgWrapper.css({opacity: 0});
 						}
@@ -585,7 +406,7 @@
 							backgroundRepeat  : "no-repeat"
 						});
 
-						var imageIndex = jQuery("<span/>").addClass("tg-img-index").html((idx + 1) + "/" + imagesList.length);
+						var imageIndex = jQuery("<span/>").addClass("tg-img-index").html((idx + 1) + "/" + imagesList.length + " ");
 						var captionLabel = jQuery("<label/>").html(imageCaption).prepend(imageIndex);
 
 						if (imageCaption)
@@ -678,93 +499,4 @@
 })(jQuery);
 
 //fgnass.github.com/spin.js#v2.0.1
-!function (a, b) {"object" == typeof exports ? module.exports = b() : "function" == typeof define && define.amd ? define(b) : a.Spinner = b()}(this, function () {
-	"use strict";
-	function a(a, b) {
-		var c, d = document.createElement(a || "div");
-		for (c in b)d[c] = b[c];
-		return d
-	}
-
-	function b(a) {
-		for (var b = 1, c = arguments.length; c > b; b++)a.appendChild(arguments[b]);
-		return a
-	}
-
-	function c(a, b, c, d) {
-		var e = ["opacity", b, ~~(100 * a), c, d].join("-"), f = .01 + c / d * 100, g = Math.max(1 - (1 - a) / b * (100 - f), a), h = j.substring(0, j.indexOf("Animation")).toLowerCase(), i = h && "-" + h + "-" || "";
-		return l[e] || (m.insertRule("@" + i + "keyframes " + e + "{0%{opacity:" + g + "}" + f + "%{opacity:" + a + "}" + (f + .01) + "%{opacity:1}" + (f + b) % 100 + "%{opacity:" + a + "}100%{opacity:" + g + "}}", m.cssRules.length), l[e] = 1), e
-	}
-
-	function d(a, b) {
-		var c, d, e = a.style;
-		for (b = b.charAt(0).toUpperCase() + b.slice(1), d = 0; d < k.length; d++)if (c = k[d] + b, void 0 !== e[c])return c;
-		return void 0 !== e[b] ? b : void 0
-	}
-
-	function e(a, b) {
-		for (var c in b)a.style[d(a, c) || c] = b[c];
-		return a
-	}
-
-	function f(a) {
-		for (var b = 1; b < arguments.length; b++) {
-			var c = arguments[b];
-			for (var d in c)void 0 === a[d] && (a[d] = c[d])
-		}
-		return a
-	}
-
-	function g(a, b) {return"string" == typeof a ? a : a[b % a.length]}
-
-	function h(a) {this.opts = f(a || {}, h.defaults, n)}
-
-	function i() {
-		function c(b, c) {return a("<" + b + ' xmlns="urn:schemas-microsoft.com:vml" class="spin-vml">', c)}
-
-		m.addRule(".spin-vml", "behavior:url(#default#VML)"), h.prototype.lines = function (a, d) {
-			function f() {return e(c("group", {coordsize: k + " " + k, coordorigin: -j + " " + -j}), {width: k, height: k})}
-
-			function h(a, h, i) {b(m, b(e(f(), {rotation: 360 / d.lines * a + "deg", left: ~~h}), b(e(c("roundrect", {arcsize: d.corners}), {width: j, height: d.width, left: d.radius, top: -d.width >> 1, filter: i}), c("fill", {color: g(d.color, a), opacity: d.opacity}), c("stroke", {opacity: 0}))))}
-
-			var i, j = d.length + d.width, k = 2 * j, l = 2 * -(d.width + d.length) + "px", m = e(f(), {position: "absolute", top: l, left: l});
-			if (d.shadow)for (i = 1; i <= d.lines; i++)h(i, -2, "progid:DXImageTransform.Microsoft.Blur(pixelradius=2,makeshadow=1,shadowopacity=.3)");
-			for (i = 1; i <= d.lines; i++)h(i);
-			return b(a, m)
-		}, h.prototype.opacity = function (a, b, c, d) {
-			var e = a.firstChild;
-			d = d.shadow && d.lines || 0, e && b + d < e.childNodes.length && (e = e.childNodes[b + d], e = e && e.firstChild, e = e && e.firstChild, e && (e.opacity = c))
-		}
-	}
-
-	var j, k = ["webkit", "Moz", "ms", "O"], l = {}, m = function () {
-		var c = a("style", {type: "text/css"});
-		return b(document.getElementsByTagName("head")[0], c), c.sheet || c.styleSheet
-	}(), n = {lines: 12, length: 7, width: 5, radius: 10, rotate: 0, corners: 1, color: "#000", direction: 1, speed: 1, trail: 100, opacity: .25, fps: 20, zIndex: 2e9, className: "spinner", top: "50%", left: "50%", position: "absolute"};
-	h.defaults = {}, f(h.prototype, {spin: function (b) {
-		this.stop();
-		{
-			var c = this, d = c.opts, f = c.el = e(a(0, {className: d.className}), {position: d.position, width: 0, zIndex: d.zIndex});
-			d.radius + d.length + d.width
-		}
-		if (e(f, {left: d.left, top: d.top}), b && b.insertBefore(f, b.firstChild || null), f.setAttribute("role", "progressbar"), c.lines(f, c.opts), !j) {
-			var g, h = 0, i = (d.lines - 1) * (1 - d.direction) / 2, k = d.fps, l = k / d.speed, m = (1 - d.opacity) / (l * d.trail / 100), n = l / d.lines;
-			!function o() {
-				h++;
-				for (var a = 0; a < d.lines; a++)g = Math.max(1 - (h + (d.lines - a) * n) % l * m, d.opacity), c.opacity(f, a * d.direction + i, g, d);
-				c.timeout = c.el && setTimeout(o, ~~(1e3 / k))
-			}()
-		}
-		return c
-	}, stop                              : function () {
-		var a = this.el;
-		return a && (clearTimeout(this.timeout), a.parentNode && a.parentNode.removeChild(a), this.el = void 0), this
-	}, lines                             : function (d, f) {
-		function h(b, c) {return e(a(), {position: "absolute", width: f.length + f.width + "px", height: f.width + "px", background: b, boxShadow: c, transformOrigin: "left", transform: "rotate(" + ~~(360 / f.lines * k + f.rotate) + "deg) translate(" + f.radius + "px,0)", borderRadius: (f.corners * f.width >> 1) + "px"})}
-
-		for (var i, k = 0, l = (f.lines - 1) * (1 - f.direction) / 2; k < f.lines; k++)i = e(a(), {position: "absolute", top: 1 + ~(f.width / 2) + "px", transform: f.hwaccel ? "translate3d(0,0,0)" : "", opacity: f.opacity, animation: j && c(f.opacity, f.trail, l + k * f.direction, f.lines) + " " + 1 / f.speed + "s linear infinite"}), f.shadow && b(i, e(h("#000", "0 0 4px #000"), {top: "2px"})), b(d, b(i, h(g(f.color, k), "0 0 1px rgba(0,0,0,.1)")));
-		return d
-	}, opacity                           : function (a, b, c) {b < a.childNodes.length && (a.childNodes[b].style.opacity = c)}});
-	var o = e(a("group"), {behavior: "url(#default#VML)"});
-	return!d(o, "transform") && o.adj ? i() : j = d(o, "animation"), h
-});
+!function(a,b){"object"==typeof exports?module.exports=b():"function"==typeof define&&define.amd?define(b):a.Spinner=b()}(this,function(){"use strict";function a(a,b){var c,d=document.createElement(a||"div");for(c in b)d[c]=b[c];return d}function b(a){for(var b=1,c=arguments.length;c>b;b++)a.appendChild(arguments[b]);return a}function c(a,b,c,d){var e=["opacity",b,~~(100*a),c,d].join("-"),f=.01+100*(c/d),g=Math.max(1-(1-a)/b*(100-f),a),h=j.substring(0,j.indexOf("Animation")).toLowerCase(),i=h&&"-"+h+"-"||"";return l[e]||(m.insertRule("@"+i+"keyframes "+e+"{0%{opacity:"+g+"}"+f+"%{opacity:"+a+"}"+(f+.01)+"%{opacity:1}"+(f+b)%100+"%{opacity:"+a+"}100%{opacity:"+g+"}}",m.cssRules.length),l[e]=1),e}function d(a,b){var c,d,e=a.style;for(b=b.charAt(0).toUpperCase()+b.slice(1),d=0;d<k.length;d++)if(c=k[d]+b,void 0!==e[c])return c;return void 0!==e[b]?b:void 0}function e(a,b){for(var c in b)a.style[d(a,c)||c]=b[c];return a}function f(a){for(var b=1;b<arguments.length;b++){var c=arguments[b];for(var d in c)void 0===a[d]&&(a[d]=c[d])}return a}function g(a,b){return"string"==typeof a?a:a[b%a.length]}function h(a){this.opts=f(a||{},h.defaults,n)}function i(){function c(b,c){return a("<"+b+' xmlns="urn:schemas-microsoft.com:vml" class="spin-vml">',c)}m.addRule(".spin-vml","behavior:url(#default#VML)"),h.prototype.lines=function(a,d){function f(){return e(c("group",{coordsize:k+" "+k,coordorigin:-j+" "+-j}),{width:k,height:k})}function h(a,h,i){b(m,b(e(f(),{rotation:360/d.lines*a+"deg",left:~~h}),b(e(c("roundrect",{arcsize:d.corners}),{width:j,height:d.width,left:d.radius,top:-d.width>>1,filter:i}),c("fill",{color:g(d.color,a),opacity:d.opacity}),c("stroke",{opacity:0}))))}var i,j=d.length+d.width,k=2*j,l=2*-(d.width+d.length)+"px",m=e(f(),{position:"absolute",top:l,left:l});if(d.shadow)for(i=1;i<=d.lines;i++)h(i,-2,"progid:DXImageTransform.Microsoft.Blur(pixelradius=2,makeshadow=1,shadowopacity=.3)");for(i=1;i<=d.lines;i++)h(i);return b(a,m)},h.prototype.opacity=function(a,b,c,d){var e=a.firstChild;d=d.shadow&&d.lines||0,e&&b+d<e.childNodes.length&&(e=e.childNodes[b+d],e=e&&e.firstChild,e=e&&e.firstChild,e&&(e.opacity=c))}}var j,k=["webkit","Moz","ms","O"],l={},m=function(){var c=a("style",{type:"text/css"});return b(document.getElementsByTagName("head")[0],c),c.sheet||c.styleSheet}(),n={lines:12,length:7,width:5,radius:10,rotate:0,corners:1,color:"#000",direction:1,speed:1,trail:100,opacity:.25,fps:20,zIndex:2e9,className:"spinner",top:"50%",left:"50%",position:"absolute"};h.defaults={},f(h.prototype,{spin:function(b){this.stop();var c=this,d=c.opts,f=c.el=e(a(0,{className:d.className}),{position:d.position,width:0,zIndex:d.zIndex});if(d.radius+d.length+d.width,e(f,{left:d.left,top:d.top}),b&&b.insertBefore(f,b.firstChild||null),f.setAttribute("role","progressbar"),c.lines(f,c.opts),!j){var g,h=0,i=(d.lines-1)*(1-d.direction)/2,k=d.fps,l=k/d.speed,m=(1-d.opacity)/(l*d.trail/100),n=l/d.lines;!function o(){h++;for(var a=0;a<d.lines;a++)g=Math.max(1-(h+(d.lines-a)*n)%l*m,d.opacity),c.opacity(f,a*d.direction+i,g,d);c.timeout=c.el&&setTimeout(o,~~(1e3/k))}()}return c},stop:function(){var a=this.el;return a&&(clearTimeout(this.timeout),a.parentNode&&a.parentNode.removeChild(a),this.el=void 0),this},lines:function(d,f){function h(b,c){return e(a(),{position:"absolute",width:f.length+f.width+"px",height:f.width+"px",background:b,boxShadow:c,transformOrigin:"left",transform:"rotate("+~~(360/f.lines*k+f.rotate)+"deg) translate("+f.radius+"px,0)",borderRadius:(f.corners*f.width>>1)+"px"})}for(var i,k=0,l=(f.lines-1)*(1-f.direction)/2;k<f.lines;k++)i=e(a(),{position:"absolute",top:1+~(f.width/2)+"px",transform:f.hwaccel?"translate3d(0,0,0)":"",opacity:f.opacity,animation:j&&c(f.opacity,f.trail,l+k*f.direction,f.lines)+" "+1/f.speed+"s linear infinite"}),f.shadow&&b(i,e(h("#000","0 0 4px #000"),{top:"2px"})),b(d,b(i,h(g(f.color,k),"0 0 1px rgba(0,0,0,.1)")));return d},opacity:function(a,b,c){b<a.childNodes.length&&(a.childNodes[b].style.opacity=c)}});var o=e(a("group"),{behavior:"url(#default#VML)"});return!d(o,"transform")&&o.adj?i():j=d(o,"animation"),h});
